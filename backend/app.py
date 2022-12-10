cea6fb919bbf0396a8f5f0d6a69d2443f2bdc8be
bogdan124 <bogdan_bibina@yahoo.com>
2022-12-10 14:17:15 +0200

    adding

--- backend/app.py
+++ backend/app.py
@@ -16,7 +16,41 @@ def api():
         "telefon":"",
         "comment":"",
     }
-    return jsonify(object)
+    ##add data to database
+    return jsonify(object)  
+
+##create an api route that takes the object variable writen up and is a post reuqest
+@app.route('/api', methods=['get'])
+def api_post():
+    object = request.json
+    subject = object['subject']
+    locatie = object['locatie']
+    data = object['data']
+    prenume = object['prenume']
+    nume = object['nume']
+    cnp = object['cnp']
+    email = object['email']
+    telefon = object['telefon']
+    comment = object['comment']
+    
+    return jsonify({
+        "subject": subject,
+        "locatie": locatie,
+        "data": data,
+        "prenume": prenume,
+        "nume": nume,
+        "cnp": cnp,
+        "email": email,
+        "telefon": telefon,
+        "comment": comment
+    })
+
+##endpoint that is getting a post request with a tag color and is created a color pallet
+@app.route('/api/<tag>', methods=['get'])
+def api_post_color(tag):
+    object = request.json
+    color = object['color']
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0',port=8080)
