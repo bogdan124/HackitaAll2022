e4b0b19c5a3893ce91e1b453f40d3254eb7f0830
bogdan124 <bogdan_bibina@yahoo.com>
2022-12-10 13:15:29 +0200

    adding dockers

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,18 @@ app = Flask(__name__)
 
 @app.route('/', methods=['get'])
 def api():
-    return jsonify({'message': 'Hello World!'})
+    object = {
+        "subject":"",
+        "locatie":"",
+        "data":"",
+        "prenume":"",
+        "nume":"",
+        "cnp":"",
+        "email":"",
+        "telefon":"",
+        "comment":"",
+    }
+    return jsonify(object)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0',port=8080)
